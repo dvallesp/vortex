fc3aa6193e342112ae7ba289c6523271e0b4f116
valles <vapeda@alumni.uv.es>
2020-07-28 09:45:51 +0200

    Fix minor bugs in clus mock file generation

--- test/masclet_mock_files.py
+++ test/masclet_mock_files.py
@@ -6,27 +6,31 @@
 #                            Author: David Vallés                              #
 ################################################################################
 
+import os
+from scipy.io import FortranFile as FF
+import masclet_framework as masclet
+import numpy as np
 
 def write_mock_grids(it, t, nl, mdm, zeta, ndxyz, npatch, patchnx, patchny, patchnz, patchx, patchy, patchz, patchrx,
-                     patchry, patchrz, pare, digits=5):
+                     patchry, patchrz, pare, digits=5, path=''):
     with open(os.path.join(path, masclet.read_masclet.filename(it, 'g', digits)), 'w') as grids:
-        grids.write('{}\t{}\t{}\t{}\t{}'.format(it, t, nl, mdm, 0))
-        grid.write('{}'.format(zeta))
-        grids.write('{}\t{}\t{}'.format(0, ndxyz, 0))
+        grids.write('{}\t{}\t{}\t{}\t{}\n'.format(it, t, nl, mdm, 0))
+        grids.write('{}\n'.format(zeta))
+        grids.write('{}\t{}\t{}\n'.format(0, ndxyz, 0))
 
         for l in range(1, len(npatch)):
-            grids.write('{}\t{}'.format(l, npatch[l]))
-            grids.write(' ----------------- within level=           {}  -----------'.format(l))
+            grids.write('{}\t{}\n'.format(l, npatch[l]))
+            grids.write(' ----------------- within level=           {}  -----------\n'.format(l))
 
-            for ipatch in range(sum(npatch[0:ir]) + 1, sum(npatch[0:ir + 1]) + 1):
-                grids.write('{}\t{}\t{}'.format(patchnx[ipatch], patchny[ipatch], patchnz[ipatch]))
-                grids.write('{}\t{}\t{}'.format(patchx[ipatch], patchy[ipatch], patchz[ipatch]))
-                grids.write('{}\t{}\t{}'.format(patchrx[ipatch], patchry[ipatch], patchrz[ipatch]))
-                grid.write('{}'.format(pare[ipatch]))
+            for ipatch in range(sum(npatch[0:l]) + 1, sum(npatch[0:l + 1]) + 1):
+                grids.write('{}\t{}\t{}\n'.format(patchnx[ipatch], patchny[ipatch], patchnz[ipatch]))
+                grids.write('{}\t{}\t{}\n'.format(patchx[ipatch], patchy[ipatch], patchz[ipatch]))
+                grids.write('{}\t{}\t{}\n'.format(patchrx[ipatch], patchry[ipatch], patchrz[ipatch]))
+                grids.write('{}\n'.format(pare[ipatch]))
 
 
-def write_mock_clus(vx, vy, vz, it, digits=5):
-    with FF(os.path.join(path, masclet.read_masclet.filename(it, 'g', digits)), 'w') as clus:
+def write_mock_clus(vx, vy, vz, it, digits=5, path=''):
+    with FF(os.path.join(path, masclet.read_masclet.filename(it, 'b', digits)), 'w') as clus:
         clus.write_record(np.array([0]).astype('i4')) #first line
         # base level
         clus.write_record(np.array([0]).astype('i4'))
