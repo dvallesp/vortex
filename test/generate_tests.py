71648dd74066c604ae399dc8b17bea4a3d431344
valles <vapeda@alumni.uv.es>
2020-07-28 13:33:45 +0200

    Add constat divergence and constant curl tests (1a and 1b)

--- test/generate_tests.py
+++ test/generate_tests.py
@@ -17,14 +17,40 @@ import masclet_framework as masclet
 from cython_fortran_file import FortranFile as FF
 
 # modules in this package
-from masclet_mock_files import write_mock_grids, write_mock_clus
-from masclet_mock_grids import mock_gridsA
-from masclet_mock_velocityfields import constant_curl_field
+from masclet_mock_files import *
+from masclet_mock_grids import *
+from masclet_mock_velocityfields import *
 
 mockit = 4000
 ncores = 8
 
-# test1b: constant curl field
+#######################################
+# test1a: constant divergence field
+#######################################
+output_path = os.path.join(initialpath, 'test_files/1a')
+maxl = 10
+nmax = 128
+size=1
+
+npatch, patchnx, patchny, patchnz, patchx, patchy, patchz, patchrx, patchry, patchrz, pare = mock_gridsA(nlevels=maxl,
+                                                                                                         Nx=nmax,
+                                                                                                         L=size)
+
+write_mock_grids(mockit, 100, maxl, 1e-10, 0.1, 100000, npatch, patchnx, patchny, patchnz, patchx, patchy, patchz,
+                 patchrx, patchry, patchrz, pare, digits=5, path=output_path)
+
+cellsrx, cellsry, cellsrz = masclet.tools_xyz.compute_position_fields(patchnx, patchny, patchnz, patchrx, patchry,
+                                                                      patchrz, npatch, size, nmax, ncores=ncores)
+
+vx, vy, vz = constant_div_field(cellsrx, cellsry, cellsrz, 0.01)
+
+write_mock_clus(vx, vy, vz, mockit, digits=5, path=output_path)
+
+os.chdir(initialpath)
+
+#######################################
+# test1a: constant curl field
+#######################################
 output_path = os.path.join(initialpath, 'test_files/1b')
 maxl = 10
 nmax = 128
--- test/masclet_mock_velocityfields.py
+++ test/masclet_mock_velocityfields.py
@@ -15,3 +15,13 @@ def constant_curl_field(cellsrx, cellsry, cellsrz, constant):
     vy = [- vi * x / rho for vi, x, rho in zip(v, cellsrx, cellsrho)]
     vz = [np.zeros(vxi.shape) for vxi in vx]
     return vx, vy, vz
+
+
+def constant_div_field(cellsrx, cellsry, cellsrz, constant):
+    cellsr = [np.sqrt(cellrx ** 2 + cellry ** 2 + cellrz ** 2) for cellrx, cellry, cellrz in zip(cellsrx, cellsry,
+                                                                                                 cellsrz)]
+    v = [constant * r for r in cellsr]
+    vx = [vi * x / r for vi, x, r in zip(v, cellsrx, cellsr)]
+    vy = [vi * y / r for vi, y, r in zip(v, cellsry, cellsr)]
+    vz = [vi * z / r for vi, z, r in zip(v, cellsrz, cellsr)]
+    return vx, vy, vz
