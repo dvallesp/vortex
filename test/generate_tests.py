7becc07c909d7182e9bb45d16049414ad873a9f0
valles <vapeda@alumni.uv.es>
2020-07-29 14:15:27 +0200

    Fix output path for test files

--- test/generate_tests.py
+++ test/generate_tests.py
@@ -31,7 +31,7 @@ verbose = True
 if verbose:
     print('Starting with test 1a')
 
-output_path = os.path.join(initialpath, 'test_files/1a')
+output_path = os.path.join(initialpath, 'test_files/1a/simu_masclet')
 maxl = 10
 nmax = 128
 size=1
@@ -51,6 +51,10 @@ if verbose:
 
 cellsrx, cellsry, cellsrz = masclet.tools_xyz.compute_position_fields(patchnx, patchny, patchnz, patchrx, patchry,
                                                                       patchrz, npatch, size, nmax, ncores=ncores)
+#if verbose: #debug
+#    levels = masclet.tools.create_vector_levels(npatch)
+#    for ipatch in range(len(cellsrx)):
+#        print(ipatch, cellsrx[ipatch].min() - size/nmax/2**(levels[ipatch]+1), cellsrx[ipatch].max() + size/nmax/2**(levels[ipatch]+1))
 
 vx, vy, vz = constant_div_field(cellsrx, cellsry, cellsrz, 0.01)
 
@@ -67,7 +71,7 @@ if verbose:
 if verbose:
     print('Starting with test 1b')
 
-output_path = os.path.join(initialpath, 'test_files/1b')
+output_path = os.path.join(initialpath, 'test_files/1b/simu_masclet')
 maxl = 10
 nmax = 128
 size=1
