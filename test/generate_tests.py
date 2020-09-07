c9c7c1486b4780c44ba1f33d5e6d1ae6b50261a2
valles <vapeda@alumni.uv.es>
2020-09-07 11:24:42 +0200

    Modify tests 1a and 1b to make use of mock grids Aprime

--- test/generate_tests.py
+++ test/generate_tests.py
@@ -39,9 +39,8 @@ size = 1
 if verbose:
     print('Grids')
 
-npatch, patchnx, patchny, patchnz, patchx, patchy, patchz, patchrx, patchry, patchrz, pare = mock_gridsA(nlevels=maxl,
-                                                                                                         Nx=nmax,
-                                                                                                         L=size)
+npatch, patchnx, patchny, patchnz, patchx, patchy, patchz, patchrx, \
+    patchry, patchrz, pare = mock_gridsAprime(nlevels=maxl, Nx=nmax, L=size)
 
 write_mock_grids(mockit, 100, maxl, 1e-10, 0.1, 100000, npatch, patchnx, patchny, patchnz, patchx, patchy, patchz,
                  patchrx, patchry, patchrz, pare, digits=5, path=output_path)
@@ -51,10 +50,6 @@ if verbose:
 
 cellsrx, cellsry, cellsrz = masclet.tools_xyz.compute_position_fields(patchnx, patchny, patchnz, patchrx, patchry,
                                                                       patchrz, npatch, size, nmax, ncores=ncores)
-#if verbose: #debug
-#    levels = masclet.tools.create_vector_levels(npatch)
-#    for ipatch in range(len(cellsrx)):
-#        print(ipatch, cellsrx[ipatch].min() - size/nmax/2**(levels[ipatch]+1), cellsrx[ipatch].max() + size/nmax/2**(levels[ipatch]+1))
 
 vx, vy, vz = constant_div_field(cellsrx, cellsry, cellsrz, 0.01)
 
@@ -74,14 +69,13 @@ if verbose:
 output_path = os.path.join(initialpath, 'test_files/1b/simu_masclet')
 maxl = 10
 nmax = 128
-size=1
+size = 1
 
 if verbose:
     print('Grids')
 
-npatch, patchnx, patchny, patchnz, patchx, patchy, patchz, patchrx, patchry, patchrz, pare = mock_gridsA(nlevels=maxl,
-                                                                                                         Nx=nmax,
-                                                                                                         L=size)
+npatch, patchnx, patchny, patchnz, patchx, patchy, patchz, patchrx, \
+    patchry, patchrz, pare = mock_gridsAprime(nlevels=maxl, Nx=nmax, L=size)
 
 write_mock_grids(mockit, 100, maxl, 1e-10, 0.1, 100000, npatch, patchnx, patchny, patchnz, patchx, patchy, patchz,
                  patchrx, patchry, patchrz, pare, digits=5, path=output_path)
