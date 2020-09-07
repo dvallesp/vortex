79fcc7bcd93af6339b6263668a08a7014887c74a
valles <vapeda@alumni.uv.es>
2020-09-07 11:21:28 +0200

    Add new mock grid to tests (labelled Aprime)

--- test/masclet_mock_grids.py
+++ test/masclet_mock_grids.py
@@ -10,6 +10,9 @@ import numpy as np
 
 
 def mock_gridsA(nlevels, Nx, L=1):
+    '''
+    Subsequently refines the grid near the origin, 8 patches per refinement level (each in one of the eight octants)
+    '''
     npatch = np.zeros(nlevels + 1, dtype='i4')
     npatch[1:] = 8
     numpatches = 8 * nlevels + 1
@@ -44,10 +47,66 @@ def mock_gridsA(nlevels, Nx, L=1):
                     patchnx[ipatch] = Nx / 2
                     patchny[ipatch] = Nx / 2
                     patchnz[ipatch] = Nx / 2
-                    
+
                     patchx[ipatch] = (patchnx[ipatch] / 2 * (i + 1)) % patchnx[ipare] + 1
                     patchy[ipatch] = (patchny[ipatch] / 2 * (j + 1)) % patchny[ipare] + 1
                     patchz[ipatch] = (patchnz[ipatch] / 2 * (k + 1)) % patchnz[ipare] + 1
+
+                    patchrx[ipatch] = patchrx[ipare] + (patchx[ipatch] - 3 / 2) * L / Nx / 2 ** (l - 1)
+                    patchry[ipatch] = patchry[ipare] + (patchy[ipatch] - 3 / 2) * L / Nx / 2 ** (l - 1)
+                    patchrz[ipatch] = patchrz[ipare] + (patchz[ipatch] - 3 / 2) * L / Nx / 2 ** (l - 1)
+
+    return npatch, patchnx, patchny, patchnz, patchx, patchy, patchz, patchrx, patchry, patchrz, pare
+
+
+def mock_gridsAprime(nlevels, Nx, L=1):
+    '''
+    Subsequently refines the grid. First level: 8 patches, central 1/8 volume of each of the octants. Subsequent
+    levels: 1/8 central volume of the parent patch
+    '''
+    npatch = np.zeros(nlevels + 1, dtype='i4')
+    npatch[1:] = 8
+    numpatches = 8 * nlevels + 1
+    patchnx = np.zeros(numpatches, dtype='i4')
+    patchny = np.zeros(numpatches, dtype='i4')
+    patchnz = np.zeros(numpatches, dtype='i4')
+    patchx = np.zeros(numpatches, dtype='i4')
+    patchy = np.zeros(numpatches, dtype='i4')
+    patchz = np.zeros(numpatches, dtype='i4')
+    patchrx = np.zeros(numpatches, dtype='f4')
+    patchry = np.zeros(numpatches, dtype='f4')
+    patchrz = np.zeros(numpatches, dtype='f4')
+    pare = np.zeros(numpatches, dtype='i4')
+
+    patchnx[0] = Nx
+    patchny[0] = Nx
+    patchnz[0] = Nx
+    patchrx[0] = - L / 2 + L / Nx
+    patchry[0] = - L / 2 + L / Nx
+    patchrz[0] = - L / 2 + L / Nx
+
+    ipatch = 0
+    for l in range(1, nlevels + 1):
+        for i in range(2):
+            for j in range(2):
+                for k in range(2):
+                    ipatch += 1
+
+                    ipare = max(0, ipatch - 8)
+                    pare[ipatch] = ipare
+
+                    patchnx[ipatch] = Nx / 2
+                    patchny[ipatch] = Nx / 2
+                    patchnz[ipatch] = Nx / 2
+
+                    if l == 1:
+                        patchx[ipatch] = 17 + i * 64
+                        patchy[ipatch] = 17 + j * 64
+                        patchz[ipatch] = 17 + k * 64
+                    else:
+                        patchx[ipatch] = 17
+                        patchy[ipatch] = 17
+                        patchz[ipatch] = 17
 
                     patchrx[ipatch] = patchrx[ipare] + (patchx[ipatch] - 3 / 2) * L / Nx / 2 ** (l - 1)
                     patchry[ipatch] = patchry[ipare] + (patchy[ipatch] - 3 / 2) * L / Nx / 2 ** (l - 1)
