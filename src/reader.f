289d318471bdca3e078936ae4dacfa72bf457a27
valles <vapeda@alumni.uv.es>
2021-04-23 10:32:59 +0200

    Add reader for Masclet's DM particles

--- src/reader.f
+++ src/reader.f
@@ -251,3 +251,184 @@ c           SOLAP(:,:,:,I)=SCR4_INT(:,:,:)
 
       RETURN
       END
+
+
+***********************************************************************
+       SUBROUTINE LEER_PARTICLES(ITER,NX,NY,NZ,T,ZETA,NL,NPATCH,
+     &            PARE,PATCHNX,PATCHNY,PATCHNZ,PATCHX,PATCHY,PATCHZ,
+     &            PATCHRX,PATCHRY,PATCHRZ)
+***********************************************************************
+*     Reads the DM particles of the simulation, builds a set of AMR
+*     grids and interpolates a continuous velocity field.
+*     This subroutine may be changed for different simulation codes.
+*     In particular, after mesh-building and interpolation, in any
+*     case we'll need to feed the main code with:
+****  GRIDS info:
+*     NPATCH: number of patches per refinement level
+*     PATCHNX, PATCHNY, PATCHNZ: cell extensions of each refinement patch
+*     PATCHX, PATCHY, PATCHZ: grid coordinates of the leftmost cell of each patch
+*     PATCHRX, PATCHRY, PATCHRZ: origin position of each patch (position of the leftmost "mother" cell)
+*     PARE: coarser patch a given patch is embedded in
+****  CLUS info:
+*     U2, U3, U4: initial velocity field (base level, i.e coarse grid)
+*     U12, U13, U14: initial velocity field (refinement patches)
+*     CR0AMR: whether a cell is refined (=0) or it isn't (=1)
+***********************************************************************
+
+       IMPLICIT NONE
+
+       INCLUDE 'vortex_parameters.dat'
+
+       INTEGER NX,NY,NZ,ITER,NDXYZ,LOW1, LOW2
+       real T,AAA,BBB,CCC,MAP,ZETA
+       INTEGER I,J,K,IX,NL,IR,IRR,N1,N2,N3
+
+       INTEGER FLAG_VERBOSE, FLAG_W_DIVROT, FLAG_W_POTENTIALS,
+     &         FLAG_W_VELOCITIES
+       COMMON /FLAGS/ FLAG_VERBOSE, FLAG_W_DIVROT, FLAG_W_POTENTIALS,
+     &                FLAG_W_VELOCITIES
+
+       INTEGER NPATCH(0:NLEVELS),PARE(NPALEV)
+       integer NPART(0:NLEVELS)
+       INTEGER PATCHNX(NPALEV),PATCHNY(NPALEV),PATCHNZ(NPALEV)
+       INTEGER PATCHX(NPALEV),PATCHY(NPALEV),PATCHZ(NPALEV)
+       real PATCHRX(NPALEV),PATCHRY(NPALEV),PATCHRZ(NPALEV)
+
+       CHARACTER*9 FILNOM1,FILNOM2, FILNOM4
+       CHARACTER*10 FILNOM3
+
+       CHARACTER*24 FIL1,FIL2,FIL4
+       CHARACTER*25 FIL3
+
+!      this might be only for testing
+       real U1(1:NMAX,1:NMAY,1:NMAZ)
+       real U11(1:NAMRX,1:NAMRY,1:NAMRZ,NPALEV)
+       common /dens/ u1,u11
+
+       real U2(0:NMAX+1,0:NMAY+1,0:NMAZ+1)
+       real U3(0:NMAX+1,0:NMAY+1,0:NMAZ+1)
+       real U4(0:NMAX+1,0:NMAY+1,0:NMAZ+1)
+       real U12(0:NAMRX+1,0:NAMRY+1,0:NAMRZ+1,NPALEV)
+       real U13(0:NAMRX+1,0:NAMRY+1,0:NAMRZ+1,NPALEV)
+       real U14(0:NAMRX+1,0:NAMRY+1,0:NAMRZ+1,NPALEV)
+       COMMON /VELOC/ U2,U3,U4,U12,U13,U14
+
+!      Particles
+       REAL*4 RXPA(NDM),RYPA(NDM),RZPA(NDM),
+     &        U2DM(NDM),U3DM(NDM),U4DM(NDM),MASAP(NDM)
+
+       integer cr0amr(1:NMAX,1:NMAY,1:NMAZ)
+       integer cr0amr1(1:NAMRX,1:NAMRY,1:NAMRZ,NPALEV)
+       common /cr0/ cr0amr, cr0amr1
+*      ---PARALLEL---
+       INTEGER NUM,OMP_GET_NUM_THREADS,NUMOR, FLAG_PARALLEL
+       COMMON /PROCESADORES/ NUM
+
+*      READING DATA
+       CALL NOMFILE(ITER,FILNOM1,FILNOM2,FILNOM3)
+       WRITE(*,*) 'Reading iteration: ',ITER,' ',FILNOM1, ' ', FILNOM2, ' ',
+     &            FILNOM3
+
+       FIL1='simu_masclet/'//FILNOM1
+       FIL2='simu_masclet/'//FILNOM2
+       FIL3='simu_masclet/'//FILNOM3
+
+       OPEN (33,FILE=FIL3,STATUS='UNKNOWN',ACTION='READ')
+
+*      pointers and general data (grids file)
+       READ(33,*) IRR,T,NL,MAP
+       READ(33,*) ZETA
+       READ(33,*) IR,NPART(0)
+       WRITE(*,*) 'IR,NPART(IR)=', IR,NPART(IR)
+       DO IR=1,NL
+       READ(33,*) IRR,NPATCH(IR),NPART(IR)
+       READ(33,*)
+       WRITE(*,*) 'IR,NPATCH(IR),NPART(IR)=', IR,NPATCH(IR),NPART(IR)
+
+       IF (IR.NE.IRR) WRITE(*,*)'Warning: fail in restart'
+       LOW1=SUM(NPATCH(0:IR-1))+1
+       LOW2=SUM(NPATCH(0:IR))
+       DO I=LOW1,LOW2
+        READ(33,*)
+        READ(33,*)
+        READ(33,*)
+        READ(33,*)
+       END DO
+       END DO
+       CLOSE(33)
+
+*      DARK MATTER
+       OPEN (32,FILE=FIL2,
+     &       STATUS='UNKNOWN',ACTION='READ',FORM='UNFORMATTED')
+        READ(32)
+        IR=0
+
+        LOW1=1
+        LOW2=NPART(IR)
+
+        READ(32) !(((U1(I,J,K),I=1,NX),J=1,NY),K=1,NZ)
+        READ(32) (RXPA(I),I=LOW1,LOW2)
+        READ(32) (RYPA(I),I=LOW1,LOW2)
+        READ(32) (RZPA(I),I=LOW1,LOW2)
+        READ(32) (U2DM(I),I=LOW1,LOW2)
+        READ(32) (U3DM(I),I=LOW1,LOW2)
+        READ(32) (U4DM(I),I=LOW1,LOW2)
+        READ(32) !particle ID
+        MASAP(LOW1:LOW2)=MAP
+        WRITE(*,*) 'RXPA=',MINVAL(RXPA(LOW1:LOW2)),
+     &                       MAXVAL(RXPA(LOW1:LOW2))
+        WRITE(*,*) 'RYPA=',MINVAL(RYPA(LOW1:LOW2)),
+     &                       MAXVAL(RYPA(LOW1:LOW2))
+        WRITE(*,*) 'RZPA=',MINVAL(RZPA(LOW1:LOW2)),
+     &                       MAXVAL(RZPA(LOW1:LOW2))
+        WRITE(*,*) 'U2DM=',MINVAL(U2DM(LOW1:LOW2)),
+     &                       MAXVAL(U2DM(LOW1:LOW2))
+        WRITE(*,*) 'U3DM=',MINVAL(U3DM(LOW1:LOW2)),
+     &                       MAXVAL(U3DM(LOW1:LOW2))
+        WRITE(*,*) 'U4DM=',MINVAL(U4DM(LOW1:LOW2)),
+     &                       MAXVAL(U4DM(LOW1:LOW2))
+        WRITE(*,*) 'MASAP=',MINVAL(MASAP(LOW1:LOW2)),
+     &                       MAXVAL(MASAP(LOW1:LOW2))
+
+       DO IR=1,NL
+        LOW1=SUM(NPATCH(0:IR-1))+1
+        LOW2=SUM(NPATCH(0:IR))
+        DO I=LOW1,LOW2
+         READ(32)
+        END DO
+        LOW1=SUM(NPART(0:IR-1))+1
+        LOW2=SUM(NPART(0:IR))
+        WRITE(*,*) 'LEVEL,LOW1,LOW2=',IR,LOW1,LOW2
+        READ(32) (RXPA(I),I=LOW1,LOW2)
+        READ(32) (RYPA(I),I=LOW1,LOW2)
+        READ(32) (RZPA(I),I=LOW1,LOW2)
+        READ(32) (U2DM(I),I=LOW1,LOW2)
+        READ(32) (U3DM(I),I=LOW1,LOW2)
+        READ(32) (U4DM(I),I=LOW1,LOW2)
+        READ(32) (MASAP(I),I=LOW1,LOW2)
+        READ(32) ! particle ID
+
+        WRITE(*,*) 'RXPA=',MINVAL(RXPA(LOW1:LOW2)),
+     &                       MAXVAL(RXPA(LOW1:LOW2))
+        WRITE(*,*) 'RYPA=',MINVAL(RYPA(LOW1:LOW2)),
+     &                       MAXVAL(RYPA(LOW1:LOW2))
+        WRITE(*,*) 'RZPA=',MINVAL(RZPA(LOW1:LOW2)),
+     &                       MAXVAL(RZPA(LOW1:LOW2))
+        WRITE(*,*) 'U2DM=',MINVAL(U2DM(LOW1:LOW2)),
+     &                       MAXVAL(U2DM(LOW1:LOW2))
+        WRITE(*,*) 'U3DM=',MINVAL(U3DM(LOW1:LOW2)),
+     &                       MAXVAL(U3DM(LOW1:LOW2))
+        WRITE(*,*) 'U4DM=',MINVAL(U4DM(LOW1:LOW2)),
+     &                       MAXVAL(U4DM(LOW1:LOW2))
+        WRITE(*,*) 'MASAP=',MINVAL(MASAP(LOW1:LOW2)),
+     &                       MAXVAL(MASAP(LOW1:LOW2))
+       END DO
+
+       CLOSE(32)
+
+*       CALL CREATE_MESH(NX,NY,NZ,NL_PARTICLE_GRID,NPATCH,
+*     &            PARE,PATCHNX,PATCHNY,PATCHNZ,PATCHX,PATCHY,PATCHZ,
+*     &            PATCHRX,PATCHRY,PATCHRZ)
+
+       RETURN
+       END
